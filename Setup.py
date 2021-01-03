45aaaa2f4ab6906df4b8a8099f4515a55bbc4ff9
hme0354 <58149131+hme0354@users.noreply.github.com>
2021-01-03 19:27:15 +0100

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -224,7 +224,7 @@ curl -q "http://$ip:8083/fhem?cmd=define%20comfoconnect%20dummy"
 echo
 echo	"Symbol erstellen"
 
-curl -q "http://192.168.0.175:8083/fhem?cmd=attr%20comfoconnect%20devStateIcon%20%7B%22%2E%2A%3Avent_ventilation_level_%22%2EReadingsVal(%22comfoconnect%22%2C%22Stufe%22%2C0)%2E(ReadingsVal(%22comfoconnect%22%2C%22Modus%22%2C0)%20ne%20%2D1%20%3F%20%27%40green%27%20%3A%20%22%22)%7D"
+curl -q "http://$ip:8083/fhem?cmd=attr%20comfoconnect%20devStateIcon%20%7B%22%2E%2A%3Avent_ventilation_level_%22%2EReadingsVal(%22comfoconnect%22%2C%22Stufe%22%2C0)%2E(ReadingsVal(%22comfoconnect%22%2C%22Modus%22%2C0)%20ne%20%2D1%20%3F%20%27%40green%27%20%3A%20%22%22)%7D"
 
 echo
 echo	"Einstellungen vornehmen"
